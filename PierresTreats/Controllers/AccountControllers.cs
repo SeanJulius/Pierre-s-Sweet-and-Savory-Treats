2e42c1bdc4c66fb684846c325ae5b80ce335509a
Sean <crimsonsand@live.com>
2022-08-27 13:09:59 -0700

    Add Startup.cs code and start on account controller

---
+++ PierresTreats/Controllers/AccountControllers.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+using PierresTreats.Models;
+using PierresTreats.ViewModels;
+
+namespace PierresTreats.Controllers
+{
+  
+}
