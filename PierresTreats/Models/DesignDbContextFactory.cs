ac53e8ec3844c97a0db9f1359f22e6022526e1db
Sean <crimsonsand@live.com>
2022-08-28 04:35:20 -0700

    Add Flavors AddTreat.cshtml code

---
+++ PierresTreats/Models/DesignDbContextFactory.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace PierresTreats.Models
+{
+  public class PierresTreatsContextFactory : IDesignDbContextFactory<PierresTreatsContext>
+  {
+
+
+    PierresTreatsContext IDesignDbContextFactory<PierresTreatsContext>.CreateDbContext(string[] args)
+    {
+
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+
+      var builder = new DbContextOptionsBuilder<PierresTreatsContext>();
+
+      builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
+
+      return new PierresTreatsContext(builder.Options);
+
+    }
+  }
+}
