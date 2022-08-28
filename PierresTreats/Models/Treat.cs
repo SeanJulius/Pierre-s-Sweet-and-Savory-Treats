dbc27459bc007bbce5a135d2765e248eecafe12a
Sean <crimsonsand@live.com>
2022-08-28 05:44:26 -0700

    Add treats.cs code

---
+++ PierresTreats/Models/Treat.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace PierresTreats.Models
+{
+
+  public class Treat
+  {
+    public Treat()
+    {
+
+      this.JoinEntities = new HashSet<FlavorTreat>();
+    }
+
+    public int TreatId { get; set; }
+
+    public string Price { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Ingredients { get; set; }
+    public virtual ApplicationUser User { get; set; }
+
+    public virtual ICollection<FlavorTreat> JoinEntities { get;}
+    
+  }
+}
--- PierresTreats/ViewModels/RegisterViewModels.cs
+++ PierresTreats/ViewModels/RegisterViewModels.cs
@@ -10,11 +10,13 @@ namespace PierresTreats.ViewModels
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
         
+
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
